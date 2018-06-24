362a3a978cf27143e04162873b195b303853643a
Matthew DiLoreto <mjdiloreto@gmail.com>
2018-06-24 15:45:06 -0400

    added remote hosts to environ

--- src/environ/index.js
+++ src/environ/index.js
@@ -1,2 +1,5 @@
-export const YELPERHELPER_NODE_ADDRESS = 'http://localhost:4000'
-export const YELPERHELPER_SPRING_ADDRESS = 'http://localhost:8080';
+export const YELPERHELPER_NODE_ADDRESS = 'http://yelper-helper-api-server.herokuapp.com/'
+export const YELPERHELPER_SPRING_ADDRESS = 'http://yelper-helper-spring-server.herokuapp.com/';
+
+export const YELPERHELPER_NODE_ADDRESS1 = 'http://localhost:4000'
+export const YELPERHELPER_SPRING_ADDRESS1 = 'http://localhost:8080';
