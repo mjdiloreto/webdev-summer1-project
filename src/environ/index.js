87c6ecb628b744882474a45a00dfa4dbff36a47f
Matthew DiLoreto <mjdiloreto@gmail.com>
2018-06-24 15:59:49 -0400

    removed erroneous trailing / from environment variables

--- src/environ/index.js
+++ src/environ/index.js
@@ -1,5 +1,5 @@
-export const YELPERHELPER_NODE_ADDRESS = 'http://yelper-helper-api-server.herokuapp.com/'
-export const YELPERHELPER_SPRING_ADDRESS = 'http://yelper-helper-spring-server.herokuapp.com/';
+export const YELPERHELPER_NODE_ADDRESS = 'http://yelper-helper-api-server.herokuapp.com';
+export const YELPERHELPER_SPRING_ADDRESS = 'http://yelper-helper-spring-server.herokuapp.com';
 
 export const YELPERHELPER_NODE_ADDRESS1 = 'http://localhost:4000'
 export const YELPERHELPER_SPRING_ADDRESS1 = 'http://localhost:8080';
